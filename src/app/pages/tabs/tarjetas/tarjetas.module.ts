ddb0d31b2757d980f49e03b7ffeb38febb13bc2c
Rocio <rocioalvarez7253@gmail.com>
2023-11-20 16:18:52 -0300

    trans-setting

--- src/app/pages/tabs/tarjetas/tarjetas.module.ts
+++ src/app/pages/tabs/tarjetas/tarjetas.module.ts
@@ -14,6 +14,7 @@ import { TarjetasPage } from './tarjetas.page';
     FormsModule,
     IonicModule,
     TarjetasPageRoutingModule
+
   ],
   declarations: [TarjetasPage]
 })
--- src/app/pages/tabs/transferencias/transferencias.page.ts
+++ src/app/pages/tabs/transferencias/transferencias.page.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-transferencias',
   templateUrl: './transferencias.page.html',
   styleUrls: ['./transferencias.page.scss'],
 })
-export class TransferenciasPage implements OnInit {
+export class TransferenciasPage {
+  allTransactions: any[]=[];
+  transactions: any[]=[];
+  segmentValue='in';
 
-  constructor() { }
-
+  constructor() {}
+  
   ngOnInit() {
+    this.allTransactions = [
+
+      { id: 1, to: 'Piyush Ag.', date: '2022-05-22', amount: 5000 },
+      { id: 2, to: 'Avinash', date:'2022-03-02', amount: 7000 },
+      { id: 3, to: 'Catherine', date: '2022-07-28', amount: -3250 },
+      { id: 4, to: 'Akhil Ag.', date:'2022-01-09', amount: 1000 },
+      { id: 5, to: 'Prem Ag.', date:'2022-04-13', amount: -800 },
+    ];
+    this.filterTransactions();
   }
 
+  filterTransactions() {
+    if(this.segmentValue == 'in') {
+      this.transactions = this.allTransactions.filter(x => x.ameunt >= 0);
+    
+    } 
+    else {
+      this.transactions = this.allTransactions.filter(x => x.amount < 0); 
+    }
+  }
+  
+  segmentChanged(event: any) {
+    console.log(event);
+    this.segmentValue=event.detail.value;
+    this.filterTransactions();
+  }
+
+
+
+  
+
+  
 }
